1a0f6e403f9e4987c956f65efd25ecab27299b11
Vakho-web <vakhogvishiani@gmail.com>
2023-07-11 16:29:50 +0400

    ADD buttons to document.html, ADD script to capture player choice based on button clicked to script.js

--- script.js
+++ script.js
@@ -1,57 +1,40 @@
-// Pseudocode
-// generate random number between 1 and 3 
-// based on the generated number computer will pick the selection
+// Computer random choice
+function getRandomChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
 
-    // if randomNumber === 1 return string rock
-    // if randomNumber === 2 return string paper, 
-    // if randomNumber === 3 return string scissors
-
-    function getRandomChoice() {
-        const choices = ["rock", "paper", "scissors"];
-        const randomIndex = Math.floor(Math.random() * choices.length);
-        return choices[randomIndex];
+// Battle function to compare choices
+function battle(computerSelection, playerSelection) {
+    if (computerSelection === playerSelection) {
+        document.getElementById("result").innerText = "Result: " + "It's a tie";
+    } else if (
+        (computerSelection === "rock" && playerSelection === "paper") ||
+        (computerSelection === "scissors" && playerSelection === "rock") ||
+        (computerSelection === "paper" && playerSelection === "scissors")
+    ) {
+        document.getElementById("result").innerText = "Result: " + "You win! " + playerSelection + " beats " + computerSelection;
+    } else if (
+        (computerSelection === "paper" && playerSelection === "rock") ||
+        (computerSelection === "rock" && playerSelection === "scissors") ||
+        (computerSelection === "scissors" && playerSelection === "paper")
+    ) {
+        document.getElementById("result").innerText = "Result: " + "You lose! " + computerSelection + " beats " + playerSelection;
+    } else {
+        document.getElementById("result").innerText = "Result: " + "Enter Rock, Paper, or Scissors";
     }
-    
-    // console.log(computerSelection)
+}
 
+// Player choice
+function getPlayerChoice(event) {
+    var playerChoice = event.target.id;
+    var computerSelection = getRandomChoice();
+    battle(computerSelection, playerChoice);
+}
 
-// player types in their selection, (case Insensitive) rock, paper or scissors
-
-    function getPlayerChoice() {
-        const playerSelection = document.getElementById("playSel");
-        const inputValue = playerSelection.value;
-        const playerSelectionLowerCase = inputValue.toLowerCase();
-        return playerSelectionLowerCase
-    }
-    
-// compare the selection
-// if computerSelection === playerSelection => tie
-// if computerSelection === rock && playerSelection === paper => player win
-// if computerSelection === paper && playerSelection === rock => computer win
-// if computerSelection === rock && playerSelection === scissors => computer win
-// if computerSelection === scissors && playerSelection === paper => player win
-// if computerSelection === paper && playerSelection === scissors => player win
-// if computerSelection === scissors && playerSelection === paper => computer win   
-
-    function battle(computerSelection, playerSelection) {
-        computerSelection = getRandomChoice();
-        playerSelection = getPlayerChoice();
-        if (computerSelection === playerSelection) {
-            return "It's A tie";
-        } else if (
-            (computerSelection === "rock" && playerSelection === "paper") || (computerSelection === "scissors" && playerSelection === "rock") || (computerSelection === "paper" && playerSelection === "scissors")) {
-            return "You Win! " + playerSelection + " beats " + computerSelection
-        } else if (
-            (computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "scissors" && playerSelection === "paper")) {
-            return "You Lose! " + computerSelection + " beats " + playerSelection
-        } else {
-            return "Enter Rock, Paper or Scissors"
-        }
-    }
-
-    function result() {
-        document.getElementById("result").innerText = battle();
-    }
-    
-
-    
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('rock').addEventListener('click', getPlayerChoice);
+    document.getElementById('paper').addEventListener('click', getPlayerChoice);
+    document.getElementById('scissors').addEventListener('click', getPlayerChoice);
+});
