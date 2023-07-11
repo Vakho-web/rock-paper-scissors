b697f215c922eed94cb542e760b1e0b7b8518320
Vakho-web <vakhogvishiani@gmail.com>
2023-07-11 20:53:00 +0400

    ADD popup to display the winner, ADD score counter, ADD reset score functionality

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0
+let computerScore = 0
+
 // Computer random choice
 function getRandomChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -15,15 +18,20 @@ function battle(computerSelection, playerSelection) {
         (computerSelection === "paper" && playerSelection === "scissors")
     ) {
         document.getElementById("result").innerText = "Result: " + "You win! " + playerSelection + " beats " + computerSelection;
+        playerScore ++
     } else if (
         (computerSelection === "paper" && playerSelection === "rock") ||
         (computerSelection === "rock" && playerSelection === "scissors") ||
         (computerSelection === "scissors" && playerSelection === "paper")
     ) {
         document.getElementById("result").innerText = "Result: " + "You lose! " + computerSelection + " beats " + playerSelection;
+        computerScore ++
     } else {
         document.getElementById("result").innerText = "Result: " + "Enter Rock, Paper, or Scissors";
     }
+
+    updateScore()
+    checkWinner()
 }
 
 // Player choice
@@ -37,4 +45,32 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('rock').addEventListener('click', getPlayerChoice);
     document.getElementById('paper').addEventListener('click', getPlayerChoice);
     document.getElementById('scissors').addEventListener('click', getPlayerChoice);
+    document.querySelector('.popup .closeBtn').addEventListener('click', closePopup);
 });
+
+function resetBtn() {
+    playerScore = 0
+    computerScore = 0
+    updateScore()
+}
+
+function updateScore() {
+    document.getElementById('playerScore').innerText = "Player Score: " + playerScore;
+    document.getElementById('computerScore').innerText = "Computer Score: " + computerScore;
+}
+
+function checkWinner() {
+    if(playerScore === 3) {
+        document.body.classList.add('active-popup');
+        document.getElementById('winner').innerText = "You have defeated the wretched computer!"
+    } else if (computerScore === 3) {
+        document.body.classList.add('active-popup')
+        document.getElementById('winner').innerText = "You have been outsmarted by random chance!"
+    }
+
+}
+
+function closePopup() {
+    document.body.classList.remove('active-popup');
+    resetBtn()
+}
